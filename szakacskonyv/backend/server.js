6af6ad77ae6560fa46f77a44bcc699385ea7586e
Kewkew00 <78853822+Kewkew00@users.noreply.github.com>
2024-09-17 12:11:32 +0200

    user registration

--- szakacskonyv/backend/server.js
+++ szakacskonyv/backend/server.js
@@ -27,3 +27,52 @@ var pool = mysql.createPool({
 app.get('/', (req, res ) => {
     res.send(`API version : ${process.env.VERSION}`);
 });
+
+//user registration
+app.post('/reg', (req, res) => {
+
+    //adat ellenorzes
+    if(!req.body.name || !req.body.email || !req.body.password || !req.body.confirm ) {
+        res.status(203).send('Nem adtál meg minden kötelező adatot!');
+        return;
+    }
+
+    //jelszavak ellenőrzése
+    if (req.body.password != req.body.confirm){
+        res.status(203).send('A megadott jelszavak nem egyeznek!');
+        return;
+    }
+
+    //jelszó kritérimuainak valo megfelelés
+    if(!req.body.password.match(passwdRegExp)) {
+        res.status(203).send('A jelszó nem elég biztonságos!');
+        return;
+    }
+
+    //email ellenőrzés
+    pool.query(`SELECT * FROM users WHERE email= '${req.body.email}'`, (err, results) => {
+        if(err) {
+            res.status(500).send('Hiba történt az adatbázis elérése közben');
+            return;
+        }
+
+        //már létező email
+        if(results.length !=0) {
+            res.status(203).send('A megadott email már regiszrálva van!');
+            return;
+        }
+
+        //új user felvétel
+        pool.query(`INSERT INTO users VALUES('${uuid.v4()}', ${req.body.name}, ${req.body.email}, SHA1('${req.body.password}'), 'user')`, (err, results) => {
+            if(err) {
+                res.status(500).send('Hiba az adatbázis elérése közben');
+                return;
+            }
+
+            res.status(203).send('Sikeres regisztráció!');
+            return;
+        });
+        return;
+    });
+
+});
