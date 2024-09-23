ea23bdcb81650d4ba4930b338ae6d1861206e96e
tarrgabor <80397144+tarrgabor@users.noreply.github.com>
2024-09-23 10:11:17 +0200

    recipeCTRL.js megírva

---
+++ szakacskonyv/frontend/Controllers/recipeCTRL.js
@@ -0,0 +1,24 @@
+let recipess = [];
+
+function addRecipe(){
+    let data = {
+        date: document.querySelector('#title').value,
+        description: document.querySelector('#description').value,
+        time: document.querySelector('#time').value,
+        additions: document.querySelector('#additions').value,
+        calorie: document.querySelector('#calorie').value
+    }
+
+    axios.post(`${serverUrl}/recipes/${loggedUser[0].ID}`, data, authorize()).then(res =>{
+        alert(res.data);
+
+        if (res.status == 200){
+            document.querySelector('#title').value = null;
+            document.querySelector('#description').value = null;
+            document.querySelector('#time').value = null;
+            document.querySelector('#additions').value = null;
+            document.querySelector('#calorie').value = null;
+            getRecipes();
+        }
+    });
+}
